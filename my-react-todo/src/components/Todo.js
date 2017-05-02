2879348ee04263f9bfed7ae621f9cf0e351cb77b
janeosaur <janewie@gmail.com>
2017-05-02 11:03:03 -0700

    completed sprint 3

---
+++ my-react-todo/src/components/Todo.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class Todo extends Component {
+  render() {
+    return(
+      <p data-todos-index={this.props.todo._id}>
+        <span>{this.props.todo.body}</span>
+      </p>
+    )
+  }
+}
+
+export default Todo
+
+
+// <span>{this.props.todo.completed.toString()}</span>
+// <span>{this.props.todo.priority}</span>
