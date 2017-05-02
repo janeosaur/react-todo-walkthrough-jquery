094b4099158c269cbb9ed673236f27193a9fb2b9
janeosaur <janewie@gmail.com>
2017-05-02 09:59:34 -0700

    completed sprint 2

--- my-react-todo/src/App.js
+++ my-react-todo/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Header from './components/Header'
+import './index.css'
 
 class App extends Component {
   render() {
@@ -7,6 +8,7 @@ class App extends Component {
       <div className="App">
 
           <Header />
+          {this.props.children}
 
       </div>
     );
