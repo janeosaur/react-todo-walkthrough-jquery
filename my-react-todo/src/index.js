72d746a997b974e1fd848b575b829ba9ce5a92df
janeosaur <janewie@gmail.com>
2017-05-05 17:38:30 -0700

    created city and home components, added client side routing

--- my-react-todo/src/index.js
+++ my-react-todo/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, browserHistory} from 'react-router'
 import routes from './config/routes.js'
-// import App from './App';
+import App from './App';
 // import './index.css';
 
 // ReactDOM.render(
 //   <App />,
-//   document.getElementById('root')
+//   document.getElementById('root2')
 // );
 
 ReactDOM.render(
